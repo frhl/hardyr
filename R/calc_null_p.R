999daa0163b92334c4ffbf4e803bbe4e427f8e7e
Frederik Lassen <flassen@flassen-271.local>
2024-03-14 15:17:01 +0000

    added functional test for simulating P-values under HWE

--- R/calc_null_p.R
+++ R/calc_null_p.R
@@ -1,6 +1,35 @@
+#' Calculate Expected P-value under the Null Hypothesis for HWE
+#'
+#' Computes the expected P-value under the null hypothesis of Hardy-Weinberg Equilibrium (HWE) 
+#' for a given allele frequency and sample size.  
+#'
+#' @param N Integer, the total number of individuals in the population.
+#' @param nA Integer, the total number of occurrences of one allele in the population.
+#' @param theta Numeric, the scaling parameter used in calculating log probabilities of 
+#'   configurations under HWE. Default is 4.
+#' @param nsim Integer, the number of simulations to perform. Each simulation generates
+#'   a single P-value from the null distribution. Default value should be set by the user
+#'   depending on the context
+#' @param use_mid_p Logical, whether to apply a mid-p correction to the computed 
+#'   P-values. Default is `FALSE`.
+#'
+#' @details
+#' The function calculates the expected P-value under the null hypothesis by first determining
+#' all possible configurations of homozygous and heterozygous individuals given the total number
+#' of alleles and then calculating the log probabilities for these configurations. It takes into 
+#' account the non-uniform distribution of P-values under the null hypothesis due to varying 
+#' allele frequencies and sample sizes. This function must be applied on EACH marker seperately. 
+#'
+#' @return A numeric value representing the expected P-value under the null hypothesis of HWE. 
+#'
+#' @examples
+#' expected_p_dist <- calc_null_p(N = 100, nA = 50)
+#' print(expected_p_dist) # select one to carry forward
+#'
+#' @export
 
 
-calc_null_p <- function(N, nA, theta=4, FUN=mean){
+calc_null_p <- function(N, nA, theta=4, nsim=100){
   
   nAB <- nA
   nB <- 2 * N - nA
@@ -10,18 +39,50 @@ calc_null_p <- function(N, nA, theta=4, FUN=mean){
   # get all possible configurations
   is_odd <- as.integer(nA%%2==1)
   configurations <- seq(is_odd, nA, by=2)
-  ps <- rep(NA, length(configurations))
-
-  # get pvalues - could do implementation much cleaner by generating
-  # the full distribution and then sampling as we go along. 
-  for (i in seq_along(configurations)){
-    ps[i] <- hwe_exact_test(N, nA, configurations[i], theta=theta)
+  log_ps <- rep(NA, length(configurations))
+  start_index <- which(configurations==nAB)
+  
+  # the trick here is to get the full distribution, and evaluate all
+  # outcomes, and then take the cumualtive sum across.
+  
+  # get first probability
+  log_p_start <- calc_log_p_hets(N, nA, nAB, theta)
+  log_ps[start_index] <- log_p_start
+  
+  nABi <- nAB
+  nAAi <- nAA
+  nBBi <- nBB
+  next_log_p <- log_p_start
+  
+  # go through lower
+  for (i in rev(which(configurations<nAB))){
+    next_log_p <- next_log_p + log_prob_decrease(nAAi, nBBi, nABi, theta)
+    log_ps[i] <- next_log_p
+    nABi  <- nABi - 2
+    nAAi <- (nA - nABi) / 2
+    nBBi <- (nB - nABi) / 2
   }
-
-  # sampling probabilities
-  probs <- hwe_cond_p_het(N, nA, configurations)$p
-  sampled_ps <- sample(ps, 100, replace = TRUE,  prob = probs)
-  return(FUN(sampled_ps))
+  
+  nABi <- nAB
+  nAAi <- nAA
+  nBBi <- nBB
+  next_log_p <- log_p_start
+  
+  # go through upper
+  for (i in which(configurations>nAB)){
+    next_log_p <-  next_log_p + log_prob_increase(nAAi, nBBi, nABi, theta)
+    log_ps[i] <- next_log_p
+    nABi  <- nABi + 2
+    nAAi <- (nA - nABi) / 2
+    nBBi <- (nB - nABi) / 2
+  }
+  
+  # get cumulative sum of probabilities across configurations
+  probs <- unlist(hwe_cond_p_het(N, nA, configurations))
+  ps <- cumsum(exp(log_ps))
+  sampled_ps <- sample(ps, nsim, replace = TRUE,  prob = probs)
+  return(sampled_ps)
   
 }
+
 
--- R/hwe_cond_p_het.R
+++ R/hwe_cond_p_het.R
@@ -1,6 +1,46 @@
-
-
-# this function is taken from https://cran.r-project.org/web/packages/HardyWeinberg/index.html 
+#' Conditional Probability of Heterozygotes under HWE
+#'
+#' Calculates the conditional probability of observing a specified number of
+#' heterozygous individuals under Hardy-Weinberg Equilibrium (HWE) conditions
+#' given the total allele counts. This function is crucial for understanding
+#' genetic variation and allele distribution in populations under the assumption
+#' of HWE.
+#'
+#' @param n Integer, the total number of individuals in the population.
+#' @param nA Integer, the total number of occurrences of one allele (A) in the
+#'   population. This count includes both homozygotes (AA) and heterozygotes (AB).
+#' @param nAB Integer, the total number of heterozygous individuals (AB) in the
+#'   population.
+#'
+#' @details The function first calculates the number of AA and BB homozygotes
+#' based on given inputs. It then computes the probability expression's numerator
+#' and denominator, utilizing factorial logarithms to accommodate large numbers.
+#' The final probability is obtained by exponentiating the difference between
+#' the numerator and denominator.
+#'
+#' The Hardy-Weinberg Equilibrium principle underpins the calculation, assuming
+#' no evolutionary forces are acting on the allele frequencies (e.g., no selection,
+#' mutation, migration, or genetic drift).
+#'
+#' @return A numeric value representing the conditional probability of observing
+#' the specified number of heterozygotes (`nAB`) given the total number of
+#' individuals (`n`) and the total number of one allele (`nA`), under Hardy-Weinberg
+#' Equilibrium conditions.
+#'
+#' @examples
+#' # Calculate the probability for a population of 100 individuals,
+#' # with 50 occurrences of allele A and 20 heterozygotes.
+#' prob <- hwe_cond_p_het(100, 50, 20)
+#' print(prob)
+#'
+#' @note This function assumes that the population is in Hardy-Weinberg Equilibrium,
+#' which may not hold in all real-world populations due to evolutionary influences.
+#'
+#' @references
+#' For more information, see the documentation for the HardyWeinberg package on CRAN:
+#' https://cran.r-project.org/web/packages/HardyWeinberg/index.html
+#'
+#' @export
 
 hwe_cond_p_het <- function (n, nA, nAB) {
 
@@ -12,5 +52,5 @@ hwe_cond_p_het <- function (n, nA, nAB) {
     denom <- lfactorial(2 * n) + lfactorial(nAB) + lfactorial(0.5 * 
         (nA - nAB)) + lfactorial(0.5 * (nB - nAB))
     prob <- exp(numer - denom)
-    return(list(p = prob))
+    return(prob)
 }
